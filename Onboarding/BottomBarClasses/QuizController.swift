91634a707c2c941092f7fcd632e5af869467a5f0
lilbeb <orangeee04@gmail.com>
2023-04-02 01:58:48 +0300

    add education blocks

---
+++ Onboarding/BottomBarClasses/QuizController.swift
@@ -0,0 +1,25 @@
+//
+//  QuizController.swift
+//  Onboarding
+//
+//  Created by Элина Борисова on 02.04.2023.
+//
+
+import UIKit
+
+class QuizController: UIViewController {
+
+    @IBAction func trueButton(_ sender: Any) {
+    }
+    
+    @IBAction func falseButton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+   
+    
+}
---
+++ Onboarding/Models/QuizFiles.swift
@@ -0,0 +1,15 @@
+//
+//  QuizFiles.swift
+//  Onboarding
+//
+//  Created by Элина Борисова on 02.04.2023.
+//
+
+import Foundation
+
+import UIKit
+
+struct QuizFiles {
+    let questions = [ Question(text: "Я больше не буду пить кока колв за компьютером", answer: "Согласен")
+    ]
+}
